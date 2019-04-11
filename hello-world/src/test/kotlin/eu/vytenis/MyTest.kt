60dd12d197b76eb59e451392d7a0f8683d445e71
Vytenis Bivainis <monrealis@gmail.com>
2019-04-12 01:42:18 +0300

    Run a hello world test

---
+++ hello-world/src/test/kotlin/eu/vytenis/MyTest.kt
@@ -0,0 +1,11 @@
+package ey.vytenis
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MyTest {
+	@Test
+	fun `this is not ok`() {
+		Assertions.assertEquals("OK", "OK")
+	}
+}
