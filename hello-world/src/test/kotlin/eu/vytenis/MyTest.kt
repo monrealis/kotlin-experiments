7472b177324d4afbf7d5b8664e72cca8a499878e
Vytenis Bivainis <monrealis@gmail.com>
2019-04-12 01:48:53 +0300

    Make the test run

--- hello-world/src/test/kotlin/eu/vytenis/MyTest.kt
+++ hello-world/src/test/kotlin/eu/vytenis/MyTest.kt
@@ -1,11 +1,10 @@
-package ey.vytenis
+package eu.vytenis
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class MyTest {
-	@Test
-	fun `this is not ok`() {
-		Assertions.assertEquals("OK", "OK")
-	}
+    @Test fun testOk() {
+        Assertions.assertEquals("OK", "OK")
+    }
 }
